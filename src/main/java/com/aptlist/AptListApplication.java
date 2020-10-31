a8c05129f218f279aa35ba20cc6422dff1e60db9
Navneeth <naveen.dec19@gmail.com>
2020-10-30 17:34:07 -0700

    first commit

---
+++ src/main/java/com/aptlist/AptListApplication.java
@@ -0,0 +1,12 @@
+package com.aptlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptListApplication.class, args);
+	}
+}
